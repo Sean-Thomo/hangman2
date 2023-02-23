153f128822cd38afe2f44226b3e512802b59c143
Sean Thomo <seansthomo@gmail.com>
2023-02-23 17:17:41 +0200

    Complete iteration 2

--- Hangman.java
+++ Hangman.java
@@ -6,26 +6,26 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            ArrayList<String> wordsArray = getStringArray();
-            String word = selectRandomWord(wordsArray);
-            int wordIndex = getRandomInt(word.length());
-            char letter = word.charAt(wordIndex);
-            final String modifiedWord = word.replace(letter, '_');
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> wordsArray = getStringArray();
+        String word = selectRandomWord(wordsArray);
+        int wordIndex = getRandomInt(word.length());
+        char letter = word.charAt(wordIndex);
+        final String modifiedWord = word.replace(letter, '_');
 
-            System.out.println("Guess the word: " + modifiedWord);
-            System.out.println("Guess the missing letter: ");
-            char answer = input.next().charAt(0);
-            isCorrectLetter(letter, answer);
-            System.out.println("The word was: " + word);
-        }
+        System.out.println("Guess the word: " + modifiedWord);
+        System.out.println("Guess the missing letter: ");
+        char answer = input.next().charAt(0);
+        isCorrectLetter(letter, answer);
+        System.out.println("The word was: " + word);
     }
 
 
     static ArrayList<String> getStringArray() {
         try {
             ArrayList<String> wordsArray = new ArrayList<String>();
-            File myObj = new File("shortwords.txt");
+            String fileName = getFileName();
+            File myObj = new File(fileName);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String word = myReader.nextLine();
@@ -60,7 +60,18 @@ public class Hangman {
         if (letter == (char) answer) {
             System.out.println("Well done! You are awesome!");
         } else {
-            System.out.println("Wrong!");
+            System.out.println("Wrong! Do better next time.");
+        }
+    }
+
+    static String getFileName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Words file? [leave empty to use shortwords.txt]:");
+        String fileName = input.nextLine();
+        if (fileName.isBlank()) {
+            return "shortwords.txt";
+        } else {
+            return fileName;
         }
     }
 }
